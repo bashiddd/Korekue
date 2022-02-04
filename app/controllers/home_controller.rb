9dc3629c665532d99f8ece28f90c3aa8de7de1c4
bashi <bashi57.d@gmail.com>
2022-02-04 23:51:16 +0900

    http request

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,24 @@
+require 'net/http'
+require 'uri'
+require 'json'
 class HomeController < ApplicationController
   def top
   end
 
   def show
+
+    uri = URI.parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=26.2523872%20127.765923&radius=2500&type=restaurant&maxprice=3&language=ja&key=AIzaSyDKXFfdpoNWmL65JRpq1Ue3HC-mL38aPMA")
+    res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) {|http|
+      http.get(uri)}
+
+    korekue = JSON.parse(res.body)
+    
+    ary = Array.new
+    korekue['results'].each do |result|
+      ary.push result
+    end
+
+    @korekue = ary.sample.slice('vicinity','name')
+    
   end
 end
