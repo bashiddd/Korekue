bd0732486bb034ccee19996bc63f066828fe8bb6
bashi <bashi57.d@gmail.com>
2022-02-08 02:02:22 +0900

    edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#top'
-  get '/' => "home#top"
+  get 'top' => "home#top"
   get 'show' => "home#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
